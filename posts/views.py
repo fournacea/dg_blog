334cbf4c8f21ee3b986e47f0b37d729f9e9a1c5b
Adam Fournace <fournacea@gmail.com>
2024-03-07 14:50:01 -0500

    Initial commit

---
+++ posts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Post
+
+
+
+# Create your views here.
+def posts_list(request):
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts/posts_list.html', {'posts':posts})
+
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    print(post)
+    return render(request, 'posts/post_page.html', {'post':post})
