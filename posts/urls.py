094ea5f8215bb09ea95632842fec9ab9cadfe9b5
Adam Fournace <fournacea@gmail.com>
2024-03-20 08:58:50 -0400

    ADD logout functionality

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.posts_list, name='list'),
+    path('new_post/', views.post_new, name='new-post'),
     path('<slug:slug>', views.post_page, name='page'),
 ]
